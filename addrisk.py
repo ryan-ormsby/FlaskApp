239d8bbfaedd3b80159a5c00fb54dbc0a0cdda1c
Ryan Ormsby <ryan.ormsby@shopify.com>
2017-11-02 16:48:30 -0400

    updated removing bad code

--- addrisk.py
+++ addrisk.py
@@ -9,27 +9,12 @@ shop_url = "https://%s:%s@%s.myshopify.com/admin" % (API_KEY, PASSWORD, SHOP_NAM
 shopify.ShopifyResource.set_site(shop_url)
 shop = shopify.Shop.current()
 
-#order = shopify.Order()
-#order.email = "ryantest@test.com"
-#order.note = "python app test order"
-#order.fulfillment_status = "fulfilled"
-#order.send_receipt = True
-#order.send_fulfillment_receipt = False
-#order.line_items = [{"variant_id": 51710689299, "quantity": 1}]
-
-#success = order.save()
-#print success
-#print order.attributes
-
-# order = order.reload()
 def editorder(ordertoaddrisk):
     addrisk = shopify.OrderRisk({'order_id': ordertoaddrisk})
-    # addrisk.order_id = order.id
     addrisk.message = "This order was placed from a proxy IP"
     addrisk.recommendation = "cancel"
     addrisk.score = "1.0"
     addrisk.source = "External"
-    # addrisk.merchant_message = "This order was placed from a proxy IP"
     addrisk.display = True
     addrisk.cause_cancel = True
     addrisk.save()
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
+from os.path import join, dirname
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
